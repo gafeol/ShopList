03c6a5f7d1329bc74fb71d552f0d0c631237a784
Gabriel <gabfeol@gmail.com>
2019-08-21 10:01:46 -0300

    User serialization and Mongo  user search

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,19 +2,43 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const mongoose = require('mongoose')
 const keys = require('./keys')
+const User = require('../models/user')
 
-mongoose.connect(keys.mongodb.dbURI, ()=>{
+
+mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, ()=>{
     console.log("Connected to mongodb")
+})
+
+passport.serializeUser((user, done)=>{
+    done(null, user.id)
+})
+
+passport.deserializeUser((userId, done)=>{
+    User.findById(userId).then((user)=>{
+        done(null, user)
+    })
 })
 
 passport.use(
     new GoogleStrategy({
-        callbackURL: '/auth/google/redirect',
+        callbackURL: 'http://localhost:5000/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }, (accessToke, refreshToken, profile, done) => {
+    }, (accessToken, refreshToken, profile, done) => {
         //passport callback function
-        console.log("PROFILE", profile)
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                console.log('user is already registered')
+            }
+            else {
+                User.create({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).then((newUser) => {
+                    console.log("new user created ", newUser)
+                })
+            }
+        })
         done()
     })
 )
--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const passport = require('passport')
 const authRoutes = require('./routes/auth')
 const passportSetup = require('./config/passport-setup')
 const port = process.env.PORT || 5000;
+const cookieSession = require('cookie-session')
+const keys = require('./config/keys')
 
 app.use('/auth', authRoutes)
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+  }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res)=>{
   res.send("OI")
